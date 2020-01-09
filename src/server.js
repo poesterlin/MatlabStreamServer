f6773054437414ecb741048825968bc16e713be4
Philip Oesterlin <philip.oesterlin@gmail.com>
2020-01-09 09:35:24 +0100

    collect data to csv file

--- src/server.js
+++ src/server.js
@@ -3,12 +3,17 @@ import polka from "polka";
 import compression from "compression";
 import * as sapper from "@sapper/server";
 import io from "socket.io";
-import tmp from "tmp";
-import { writeFile, readdirSync, unlinkSync, readFileSync } from "fs";
-import { spawn } from "child_process";
+import {
+  writeFile,
+  readdirSync,
+  unlinkSync,
+  readFileSync,
+  appendFileSync,
+  writeFileSync
+} from "fs";
 import { join } from "path";
-import { createServer } from 'https';
-import * as http from 'http';
+import { createServer } from "https";
+import * as http from "http";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
@@ -16,9 +21,15 @@ const dev = NODE_ENV === "development";
 const options = {};
 
 if (!dev) {
-  options.key = readFileSync("/etc/letsencrypt/live/matlab.oesterlin.dev-0001/privkey.pem");
-  options.cert = readFileSync("/etc/letsencrypt/live/matlab.oesterlin.dev-0001/cert.pem");
-  options.ca = readFileSync("/etc/letsencrypt/live/matlab.oesterlin.dev-0001/chain.pem");
+  options.key = readFileSync(
+    "/etc/letsencrypt/live/matlab.oesterlin.dev-0001/privkey.pem"
+  );
+  options.cert = readFileSync(
+    "/etc/letsencrypt/live/matlab.oesterlin.dev-0001/cert.pem"
+  );
+  options.ca = readFileSync(
+    "/etc/letsencrypt/live/matlab.oesterlin.dev-0001/chain.pem"
+  );
 }
 
 const tmpDir = "csv";
@@ -39,66 +50,53 @@ function cleanTemp() {
 
 process.on("beforeExit", cleanTemp);
 
-const { handler } = polka().use(
-  compression({ threshold: 0 }),
-  sirv("static", { dev }),
-  sapper.middleware()
-)
+const app = polka()
+  .get("/csv", (req, res) => {
+    res.end(readFileSync(join("csv", "file.csv")));
+  })
+  .use(
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  );
 
 let create = createServer;
-if(dev){
-  create = http.createServer
+if (dev) {
+  create = http.createServer;
 }
 
-const server = create(options, handler).listen(PORT, err => {
-  if(err) throw err;
+const server = create(options, app.handler).listen(PORT, err => {
+  if (err) throw err;
   console.log(`> Running on port ${PORT}`);
 });
 
+app.get("/csv", (req, res) => {
+  console.log("csv", res.sendFile);
+  res.sendFile("csv/file.csv");
+});
+
+resetFile("");
+
+let lastTs = 0;
+
 const socket = io(server);
-socket.on("connection", function (socket) {
-  socket.on("message", function (data) {
-    saveTmp(JSON.parse(data));
+socket.on("connection", function(socket) {
+  socket.on("message", function(data) {
+    const newData = JSON.parse(data);
+    lastTs += newData.length;
+
+    const csv = newData
+      .map((line, i) => `${(i + lastTs) * 100},${line.x},${line.y},${line.z}`)
+      .join("\n");
+    if (lastTs > 600) {
+      resetFile(csv);
+    }
+    appendFileSync(join("csv", "file.csv"), "\n" + csv);
   });
 });
 
-tmp.setGracefulCleanup();
-
-function saveTmp(arr) {
-  tmp.file(
-    { prefix: "data-", postfix: ".csv", dir: tmpDir },
-    (err, path, _, cleanup) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      const header = "time x y z\n";
-      const csv = arr
-        .map((line, i) => `${i * 100},${line.x},${line.y},${line.z}`)
-        .join("\n");
-      writeFile(path, header.concat(csv), () => runMatlab(path).then(cleanup));
-    }
-  );
-}
-
-function runMatlab(path) {
-  const start = Date.now();
-
-  //TODO: update command
-  // run matlab
-  const matlabProcess = spawn("ls");
-
-  return new Promise(res => {
-    // listen to process output and send it over socket
-    matlabProcess.stdout.on("data", results => {
-      //TODO: parse results
-      socket.emit("results", results.toString().length);
-    });
-
-    // log processing times
-    matlabProcess.on("close", () => {
-      console.log("matlab done in: " + (Date.now() - start) / 1000, "s");
-      setTimeout(res, 10000);
-    });
-  });
+function resetFile(csv) {
+  const header = "time x y z\n";
+  writeFileSync(join("csv", "file.csv"), header + csv);
+  console.log("reset files");
 }
